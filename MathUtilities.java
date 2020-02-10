e64453b62ab21ef1258cc4113da3af5e5d1f6f95
Sandeep Naraya <snaraya@zipcodes-MacBook-Pro-9.local>
2020-02-10 12:19:03 -0500

    Completed mathUtilities

--- MathUtilities.java
+++ MathUtilities.java
@@ -9,7 +9,9 @@ public class MathUtilities{
    * @return the sum of the two numbers
    */
   public Integer add(Integer baseValue, Integer valueToAdd){
-      return null;
+      return baseValue + valueToAdd;
+      
+      
   }
 
   /**
@@ -19,7 +21,7 @@ public class MathUtilities{
    * @return the sum of the two numbers
    */
   public Double add(Double baseValue, Double valueToAdd){
-      return null;
+      return baseValue + valueToAdd;
   }
 
   /**
@@ -28,7 +30,9 @@ public class MathUtilities{
    * @return the half of the number in double
    */
   public Double half(Integer number) {
-      return null;
+      return (double) number/2;
+      
+     
   }
 
   /**
@@ -37,7 +41,14 @@ public class MathUtilities{
    * @return true if the number is odd, false if it is even
    */
   public Boolean isOdd(Integer number){
-      return null;
+      int i= (number%2);
+      if (i == 0)
+      {
+        return false;
+    }
+      else 
+      
+      return true ;
   }
 
 
@@ -47,7 +58,7 @@ public class MathUtilities{
    * @return the result of the number multiply by itself
    */
   public Integer square(Integer number) {
-      return null;
+      return number*number;
   }
 
 }
